d236698f97fbc96564d774515f8529aa4b5de0a2
Wichtowski <70707094+Wichtowski@users.noreply.github.com>
2021-09-30 23:13:32 +0200

    Update ThreeSum.py

--- ThreeSum.py
+++ ThreeSum.py
@@ -1,5 +1,7 @@
 import random
-def sum_three(nums):
+
+
+def sum_three(x):
     tab = []
     x.sort()
     if len(x) < 3:
@@ -15,10 +17,33 @@ def sum_three(nums):
     return res
 
 
+def optimised_sum_three(x):
+    tab = []
+    nums.sort()
+    for i, a in enumerate(nums):
+        if i > 0 and a == nums[i - 1]:
+            continue
+        left = i + 1
+        right = len(nums) - 1
+        while left < right:
+            three_sum = a + nums[left] + nums[right]
+            if three_sum > 0:
+                right -= 1
+            elif three_sum < 0:
+                left += 1
+            else:
+                tab.append([a, nums[left], nums[right]])
+                left += 1
+                while nums[left] == nums[left - 1] and left < right:
+                    left += 1
+    return tab
+
+
 len_number = random.randint(0, 11)
 nums = []
 for num in range(len_number):
     num = random.randint(-11, 11)
     nums.append(num)
 print(f"Numbers: {nums}")
-print(f"Sum of three element that sums to 0 : {sum_three(nums)}")
+print(f"Sum of three elements that equals to 0 : {sum_three(nums)}")
+print(f"Sum of three (optimised) elements that equals to 0 : {optimised_sum_three(nums)}")
