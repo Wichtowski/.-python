b7610370cd5215b7020321eada00abd22c1ef0f5
Wichtowski <70707094+Wichtowski@users.noreply.github.com>
2021-09-30 12:36:10 +0200

    Update ThreeSum.py

--- ThreeSum.py
+++ ThreeSum.py
@@ -1,23 +1,25 @@
 import random
-def sumThree(nums):
+def sum_three(nums):
     tab = []
-    nums.sort()
-    if len(nums) < 3:
+    x.sort()
+    if len(x) < 3:
         return []
     else:
-        for i in range(len(nums) - 1):
-            for j in range(i + 1, len(nums)):
+        for i in range(len(x) - 1):
+            for j in range(i + 1, len(x)):
                 if i != j:
-                    for k in range(j + 1, len(nums)):
-                        if i != k and j != k and nums[i] + nums[j] + nums[k] == 0:
-                            tab.append([nums[i], nums[j], nums[k]])
+                    for k in range(j + 1, len(x)):
+                        if i != k and j != k and x[i] + x[j] + x[k] == 0:
+                            tab.append([x[i], x[j], x[k]])
             res = list(set(tuple(sorted(sub)) for sub in tab))
 
     return res
+
+
 len_number = random.randint(0, 11)
 nums = []
 for num in range(len_number):
     num = random.randint(-11, 11)
     nums.append(num)
-print(nums)
-print(sumThree(nums))
+print(f"Numbers: {nums}")
+print(f"Sum of three element that sums to 0 : {sum_three(nums)}")
