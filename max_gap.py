3029d6460d86c0fa8a380fefe7158ad8e57e1602
Wichtowski <70707094+Wichtowski@users.noreply.github.com>
2021-10-14 13:52:04 +0200

    Create max_gap.py

---
+++ max_gap.py
@@ -0,0 +1,22 @@
+import random
+def max_gap(nums):
+    if len(nums) < 2:
+        return 0
+    else:
+        for i in range(0, len(nums)-1):
+            if i == 0:
+                result =  nums[i+1] - nums[i]
+            if result < nums[i+1] - nums[i]:
+                result =  nums[i+1] - nums[i]
+            else:
+                pass
+    return result
+
+
+y = random.randint(0,20)
+nums = []
+for i in range(0,y):
+    nums.append(random.randint(0,100))
+nums.sort()    
+print(nums)
+print(max_gap(nums))
